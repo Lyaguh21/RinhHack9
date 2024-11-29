c1085751e17f3c59e4f99660c143c96daa03b351
Nikita <nikitos54345@gmail.com>
2024-11-29 18:24:36 +0300

    фон и шрифт

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Footer from './Footer.js'
 function App() {
 	return(
 		<>
+			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com"></link>
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+				<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"></link>
+
+			</head>
 			<Header />
 			<Footer />
 		</>
--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,8 @@ function Header() {
 			<img src="../public/images/logo.png" alt="logo_image"/>
 			<div className="flex-box">
 				<div className="button">Файл</div>
+				<div className="button">Вид</div>
+				<div className="button">Формулы</div>
 				<div className="button">Справка</div>
 			</div>
 		</header>
