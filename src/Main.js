73039a4bc2b59c6c6d8d84dc33129cdea5a9ab8f
Nikita <nikitos54345@gmail.com>
2024-11-30 14:30:22 +0300

    закоментил таблицу чтобы понятно хоть что-то было

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,14 @@
 import React, {useState} from 'react'
 function Main(props){
+	
+	
+	// алфавит  для букавок
+
 	const al = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ы', 'Э', 'Ю', 'Я'];
+
+
+	// из индекса таблицы(типо "Б8") делает масивчик с индексами этой ячейки в массиве tableElements(типо "[2, 8]")
+	
 	function indexConvert(tableIndex){
 		let normIndex = ['0', '0'];	
 		if(al.includes(tableIndex[1])){
@@ -18,6 +26,10 @@ function Main(props){
 		normIndex[1] = (parseInt(tableIndex.slice(1))).toString();
 		return(normIndex);
 	}
+	
+
+	// из нормального массивчика с индексами делает табличный формат, как предыдушая функция, только наоборот короче
+	
 	function indexTabler(normIndex){
 		let tableIndex = "";
 		if(normIndex[0] > al.length * al.length){
@@ -31,18 +43,33 @@ function Main(props){
 		tableIndex = al[parseInt(normIndex[0]) - 1] + normIndex[1];
 		return(tableIndex);
 	}
+
+	//пока не уверен в нужности этого 
 	let tableList = [];  
 	for(let i=0; i<props.width; i++){
 		tableList[i]= [];
 	}
+
+
+	//масивчик с ячейками
+	
 	let tableElements = [];
+
+	// пока не работает
+	
 	function setCell(e){
 		tableList[indexConvert(e.target.id)[0]][indexConvert(e.target.id)[1]] = e.target.value;		
 		console.log(tableList);
 	}
 
+
+	//шаблон ячейки
 	var cell = "<input type='text' class='cell'></input>";
 	cell = cell.repeat(props.height * props.width);
+
+
+	// поиск ячеек
+	
 	window.onload = function(){
 		tableElements = document.querySelectorAll('input');
 		for(let i = 0; i< props.width; i++){
@@ -51,12 +78,12 @@ function Main(props){
 				tableElements.item(i*props.width + j).id=indexTabler([i+1,j+1]);
 				tableElements.item(i*props.width + j).onChange=(event)=>{setCell(event)};
 				tableList[i][j] = tableElements.item(i*props.width + j).id; 
-				console.log(i*props.width+j);
 			}
 		}
-		console.log(tableList);
 	};
 
+
+	//html фрагментик
 	return(
 		<div id="table"	dangerouslySetInnerHTML={{__html: cell}}> 	
 		</div>
